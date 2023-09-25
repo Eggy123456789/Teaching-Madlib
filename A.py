14de9f015c96513cb7fa44b5cea6d19389477346
Kevin Bai <zeyubai66@gmail.com>
2023-09-25 15:33:34 -0400

    kevin

--- A.py
+++ A.py
@@ -2,12 +2,12 @@
 # you aren't going to know what they are doing until after they push their changes
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
-year_in_the_future = 0 # assign numbers or strings to the end of these versions
-wacky_robot_name = ""
-adjective_1 = ""
-violent_verb = ""
-tech_company = ""
-dollar_amount = ""
-adjective_2 = ""
-noun_1 = ""
-large_store = ""
+year_in_the_future = 1757 # assign numbers or strings to the end of these versions
+wacky_robot_name = "Eggster"
+adjective_1 = "yellow"
+violent_verb = "punch"
+tech_company = "Microsoft"
+dollar_amount = "10000"
+adjective_2 = "smelly"
+noun_1 = "egg"
+large_store = "Walmart"
